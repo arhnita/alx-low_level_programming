7ab1dad24e2a5a474e265153ec5cd6afb7f5bed9
arhnita <migist2@gmail.com>
2022-09-09 10:57:15 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: alphabets
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int a = 97;
+	while (a <= 122)
+	putchar(a);
+	a++;
+	putchar('\n');
+	return (0);
+}
