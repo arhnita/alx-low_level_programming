c7dcee0223e0d219914374994d44bf7be9297a7b
arhnita <migist2@gmail.com>
2022-09-13 14:53:05 -0700

    display numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - display numbers from input to 98
+ * @n: integer to be tested
+ * Return: Always 0
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (n = n; n <= 97; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else
+	{
+		for (n = n; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("98\n");
+}
