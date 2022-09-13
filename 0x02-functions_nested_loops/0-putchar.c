b0d764ba395cc857d47d0f566d1b8705ca52cff1
arhnita <migist2@gmail.com>
2022-09-13 04:10:43 -0700

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,13 @@
  */
 int main(void)
 {
-	_putchar("\_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
+	char *p = "\_putchar";
+
+	while(*p)
+	{
+	 _putchar(*p);
+	 p++;
+	}
 
 	_putchar("\n");
 
