f11c75afd64a2415b63806f591dfdb18aecdc01b
arhnita <migist2@gmail.com>
2022-09-15 04:53:30 -0700

    print most numbers but not all

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints num 0-9 except 2 & 4
+ * Return: no return value
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num != 2 && num != 4)
+		{
+			_putchar((num % 10) + '0');
+		}
+		_putchar("\n");
+	}
+}
