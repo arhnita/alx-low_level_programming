8677b6e1d23119105d3ce274d81e2f322b524f65
arhnita <migist2@gmail.com>
2022-09-15 05:02:51 -0700

    print most numbers but not all

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,8 +9,14 @@ void print_most_numbers(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		if (num != 2 && num != 4)
-			_putchar((num % 10) + '0');
-		_putchar('\n');
+		if ((num == 2) || (num == 4))
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(num + '0');
+		}
 	}
+	_putchar('\n');
 }
