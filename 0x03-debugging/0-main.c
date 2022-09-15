8a571ee60f3e3dd291ef57e7b40d319b657d48f2
arhnita <migist2@gmail.com>
2022-09-15 02:09:36 -0700

    function to test prototype gives correct output

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,11 +4,13 @@
  * main - tests functions that prints if an integer is positive or negative
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
 
 	i = 98 % 2;
 	positive_or_negative(i);
+
 	return (0);
 }
