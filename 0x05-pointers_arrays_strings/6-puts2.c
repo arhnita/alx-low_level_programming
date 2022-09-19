edde2f2059bccddadd9f75490839263fc7e7fcd2
arhnita <migist2@gmail.com>
2022-09-19 08:58:08 -0700

    prints one character out of two

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - function that prints only one character out of 2
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+		{
+			_putchar(str[i]);
+		}
+		else
+		{
+			continue;
+		}
+	}
+}
