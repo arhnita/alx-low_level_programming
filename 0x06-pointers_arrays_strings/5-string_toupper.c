76a7645f8066b3fb9ce2267a19c887e42480ccfb
arhnita <migist2@gmail.com>
2022-09-22 11:46:07 -0700

    change to upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - Changes lowercase to uppercase
+ * @p: The string to be modified
+ * Return: char value
+ */
+char *string_toupper(char *p)
+{
+	int a = 0;
+
+	while (p[a])
+	{
+		if (p[a] >= 97 && p[a] <= 122)
+		{
+			p[a] -= 32;
+		}
+		a++;
+	}
+	return (p);
+}
