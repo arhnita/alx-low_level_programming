97a246400f597dd20e3cc0330d126b7c396aad0c
arhnita <migist2@gmail.com>
2022-09-22 06:01:46 -0700

    string concantenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,11 +7,15 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int i, j = 0;
 
-	while (dest[index++])
-		dest_len++;
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	while (*(dest + j) != '\0')
+		j++;
+	for (i = 0; *(src + i) != '\0'; i++)
+	{
+		*(dest + j) = *(src + i);
+		j++;
+	}
+	*(dest + j) = '\0';
 	return (dest);
 }
