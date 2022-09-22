6c1c4a77fc9f64f0cb17c8eecaaec630a56781d9
arhnita <migist2@gmail.com>
2022-09-22 01:10:51 -0700

    concantenate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,22 +7,24 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int a;
+	int b;
 
-	i = 0;
-	j = 0;
+	a = 0;
 
-	while (dest[i] != '\0')
+	while (dest[a] != 0)
 	{
-		i++;
+		a++;
 	}
-	while (src[j] != '\0')
+
+	b = 0;
+
+	while (src[b] != 0)
 	{
-		dest[i] = src[j];
-		j++;
-		i++;
+		dest[a] = src[b];
+		a++;
+		b++;
 	}
-	dest[i] = '\0';
+
 	return (dest);
 }
