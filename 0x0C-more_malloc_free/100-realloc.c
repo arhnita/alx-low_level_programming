fcfb2d542e714a1b315480a33e1b10cb695bbbcd
arhnita <migist2@gmail.com>
2022-10-06 11:18:21 -0700

    reallocate

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,41 +1,39 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 /**
- * _realloc - Reallocates a memory block using malloc and free
- * @ptr: A pointer to the memory previously allocated.
- * @old_size: The size in bytes of the allocated space for pt
- * @new_size: The size in bytes for the new memory block.
- * Return: If new_size == old_size - ptr.
- * If new_size == 0 and ptr is not NULL - NULL.
- * Otherwise - a pointer to the reallocated memory block.
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previously allocated
+ * call to ``` malloc : malloc(old_size) ```
+ * @old_size: size in bytes allocated for ptr
+ * @new_size: size in bytws of new memory block
+ * Return: pointer to new mem block, NULL or ptr
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *mem;
-	char *ptr_copy, *filler;
-	unsigned int index;
+	char *nptr;
+	unsigned int i;
 
 	if (new_size == old_size)
 		return (ptr);
-	if (ptr == NULL)
-	{
-		mem = malloc(new_size);
-
-		if (mem == NULL)
-			return (NULL);
-		return (mem);
-	}
-
-	if (new_size == 0 && ptr != NULL)
+	if ((new_size == 0) && (ptr != NULL))
 	{
 		free(ptr);
 		return (NULL);
 	}
-
-	filler = mem;
-
-	for (index = 0; index < old_size && index < new_size; index++)
-		filler[index] = *ptr_copy++;
-	free(ptr);
-	return (mem);
+	if (ptr == NULL)
+	{
+		nptr = malloc(new_size);
+		if (nptr == NULL)
+			return (NULL);
+	}
+	if (new_size > old_size && (ptr != NULL))
+	{
+		nptr = malloc(new_size);
+		if (nptr == NULL)
+			return (nptr);
+		for (i = 0; i < old_size; i++)
+			nptr[i] = *((char *)ptr + 1);
+		free(ptr);
+	}
+	return (nptr);
 }
