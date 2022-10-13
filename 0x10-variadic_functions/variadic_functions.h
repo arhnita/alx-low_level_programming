9bc0c0e0f86ecfa6e543a8847a94e95da5bdcd1c
arhnita <migist2@gmail.com>
2022-10-13 12:20:59 -0700

    sum

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef VARIABLE_FUNCTIONS_H
+#define VARIABLE_FUNCTIONS_H
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* VARIABLE_FUNCTIONS_H */
